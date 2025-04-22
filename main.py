a8f816ad86ca578720cbe7d91af93874c7e53876
liu <3330176274@qq.com>
2025-04-22 16:17:41 +0800

    finish readme; modify main.py to parse arg

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import argparse
 from deal import deal_pdf
 
 if __name__ == '__main__':
-    pdfName = r"E:\pythonProject\工大目录抽取数据集\281834793.pdf"
-    # pdfName = r"..\兴业股份：兴业股份2023年年度报告.pdf"
-    txtName = r'output\281834793.txt'
-    with open(txtName, 'w', encoding='utf-8') as fp:
-        deal_pdf(pdfName, fp)
+    parser = argparse.ArgumentParser(description='Extract text from a PDF file.')
+    parser.add_argument('-d', '--pdf_path', required=True, help='Path to the input PDF file')
+    parser.add_argument('-o', '--output_path', required=True, help='Path to the output TXT file')
+
+    args = parser.parse_args()
+
+    with open(args.output_path, 'w', encoding='utf-8') as fp:
+        deal_pdf(args.pdf_path, fp)
+    print(f"Done")
